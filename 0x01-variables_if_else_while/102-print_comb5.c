3041ca14dbb30688052f5ac4310f1dd54e8bd31f
Hailu Worku <hailuworku1@yahoo.com>
2022-03-11 15:00:27 +0300

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -5,20 +5,33 @@
  */
 int main(void)
 {
-int a;
-int b;
-int c;
-for (b = 0; b < 10; b++)
-{
-  for (a = 0; a < 10; a++)
-  {
-    
-    putchar(b +'0');
-    putchar(a +'0');
-    putchar(' ');
-    
-  }
-}
-putchar('\n');
-return (0);
+    int a;
+    int b;
+    int c;
+    int d;
+    for (d = 0; d < 100; d++)
+    {
+      b = d / 10;
+      c = d % 10;
+      putchar(b + '0');
+      putchar(c + '0');
+      if (d < 99) 
+      {
+          putchar(' ');
+      }
+      for (a = 0; a < 100; a++)
+      {
+          b = a / 10;
+          c = a % 10;
+          putchar(b + '0');
+          putchar(c + '0');
+          if (a < 99) 
+          {
+              putchar(',');
+              putchar(' ');
+          }
+      }
+    }
+    putchar('\n');
+    return (0);
 } 
