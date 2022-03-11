eeb5cf2a7c78dd4b470548a13b01da07c9fc514f
Hailu Worku <hailuworku1@yahoo.com>
2022-03-11 13:58:51 +0300

    Create 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *main - function
+ *Return: valve 0
+ */
+int main(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+{
+putchar(c);
+}
+putchar('\n');
+return (0);
+}
