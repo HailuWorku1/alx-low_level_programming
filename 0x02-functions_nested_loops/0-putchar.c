7d6ef4e6452fee6a593be8e270bec889e6be35e1
Hailu Worku <hailuworku1@yahoo.com>
2022-03-16 18:46:09 +0300

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -12,3 +12,16 @@ int _putchar(char c)
   return (write(1, &c, 1));
 }
 
+int main()
+{
+  _putchar('_');
+  _putchar('p');
+  _putchar('u');
+  _putchar('t');
+  _putchar('c');
+  _putchar('h');
+  _putchar('a');
+  _putchar('r');
+  _putchar('\n');
+  return (0);
+}
