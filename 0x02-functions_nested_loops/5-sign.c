5457a05a974998a4e416ec2d914417b419f05d69
Hailu Worku <hailuworku1@yahoo.com>
2022-03-16 18:32:24 +0300

    Update and rename 5-main.c to 5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *print_sign - @coment
+ *@n: function
+ *Return: valve 0
+ */
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar(43);
+return (1);
+}
+else if (n < 0)
+{
+_putchar(45);
+return (-1);
+}
+else
+{
+_putchar(48);
+return (0);
+}
+return (0);
+}
