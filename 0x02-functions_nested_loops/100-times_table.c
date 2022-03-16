bfaaf921b2332ccf843eac41da7dc34814a4386c
Hailu Worku <hailuworku1@yahoo.com>
2022-03-16 19:04:54 +0300

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -48,19 +48,3 @@ void print_times_table(int n)
 	}
 }
 
-/**
- * main - check the code.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    print_times_table(3);
-    _putchar('\n');
-    print_times_table(5);
-    _putchar('\n');
-    print_times_table(98);
-    _putchar('\n');
-    print_times_table(12);  
-    return (0);
-}
