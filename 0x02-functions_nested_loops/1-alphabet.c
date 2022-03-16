0b5169c5f6a25fbc55a90996bb32bebc001c8310
Hailu Worku <hailuworku1@yahoo.com>
2022-03-16 18:25:54 +0300

    Create 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *print_alphabet - function
+ *Return: valve 0
+ */
+void print_alphabet(void)
+{
+char x;
+for (x = 'a'; x <= 'z'; x++)
+{
+_putchar(x);
+}
+_putchar('\n');
+}
