a241a497285dd38d6c8b8105242dab520bfb4d2b
Hailu Worku <hailuworku1@yahoo.com>
2022-03-21 08:56:17 +0300

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,4 +1,4 @@
-// This function takes a pointer to an int as parameter and updates the value it points to to 98
+/* This function takes a pointer to an int as parameter and updates the value it points to to 98 */
 
 #include <stdio.h>
 
