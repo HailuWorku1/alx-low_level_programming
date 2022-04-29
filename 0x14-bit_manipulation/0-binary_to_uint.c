3de0b670ffd6b272e333730d60bdc5513561612a
Hailu Worku <hailuworku1@yahoo.com>
2022-04-29 08:20:36 +0300

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -2,22 +2,27 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-int i, j, x, c;
-unsigned int n = 0;
-if (b == NULL)
-return (0);
-for (i = 0; b[i] != '\0'; i++)
-{
-if (b[i] != 48 && b[i] != 49)
-return (0);
-}
-j = 1;
-for (c = i - 1; c >= 0; c--)
-{
-x = b[c] - '0';
-x = x *j;
-j = j * 2;
-n = n + x;
-}
-return (n);
+	unsigned int res = 0;
+	int base = 1, i = 0;
+
+	if (b == NULL)
+		return (0);
+
+	while (b[i + 1])
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+		i++;
+	}
+
+	while (i >= 0)
+	{
+		res += ((b[i] - '0') * base);
+		base *= 2;
+		i--;
+	}
+
+
+	return (res);
+
 }
