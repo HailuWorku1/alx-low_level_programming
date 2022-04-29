b369f65e58ccc80bea4f911b9d2860e0fdb70e85
Hailu Worku <hailuworku1@yahoo.com>
2022-04-29 08:26:07 +0300

    Update main.h

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -4,19 +4,12 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-typedef struct copy_struct
-{
-	char *from;
-	char *to;
-	ssize_t rd;
-	ssize_t wt;
-	int from_file;
-	int to_file;
-	char *buffer;
-} copy_struct;
-ssize_t read_textfile(const char *filename, size_t letters);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
-int create_file(const char *filename, char *text_content);
-int append_text_to_file(const char *filename, char *text_content);
+int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
+int get_endianness(void);
+
 #endif
